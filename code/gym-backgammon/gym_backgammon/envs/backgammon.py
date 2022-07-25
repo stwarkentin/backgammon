3c8b3f8e31342378f5f24389f515eecf86d0e630
stwarkentin <stwarkentin@uos.de>
2022-07-25 21:51:16 +0200

    Make minor changes

--- code/gym-backgammon/gym_backgammon/envs/backgammon.py
+++ code/gym-backgammon/gym_backgammon/envs/backgammon.py
@@ -3,6 +3,7 @@ from gym.spaces import Dict, Discrete
 from typing import Optional
 from gym.utils.renderer import Renderer
 import numpy as np
+from random import random
 
 class BackgammonEnv(gym.Env):
     metadata = {'render_modes': ['placeholder'], 'render_fps': 4}
@@ -18,7 +19,7 @@ class BackgammonEnv(gym.Env):
         # For each of the two players the number of that player's pieces at a given location ('boardmen') is encoded with four units
             # "The first three units encoded separately the cases of one man, two men, and three men, while the fourth unit encoded the number of men beyond 3." - *citation*
             # this is called a "truncated unary encoding": https://en.wikipedia.org/wiki/Unary_coding, http://www.scholarpedia.org/article/User:Gerald_Tesauro/Proposed/Td-gammon
-            # i.e. 0: [0,0,0,0], 1: [1,0,0,0], 2: [0,1,0,0], 3: [0,0,1,0], 3<n<=15: [0,0,0,0.5*(n-3)]
+            # i.e. 0: [0,0,0,0], 1: [1,0,0,0], 2: [1,1,0,0], 3: [1,1,1,0], 3<n<=15: [1,1,1,0.5*(n-3)]
         # That is 96 units each for a subtotal of 192 units
         # Additionally we require a subtotal of 6 units, which consist of 3 units per player to encode the number of that player's pieces on the bar ('barmen'), off the board ('men off'), and whether it is that player's turn or not ('turn)
         # This gives us a total of 198 units which make up an observation
@@ -46,29 +47,33 @@ class BackgammonEnv(gym.Env):
                 ),
                 'Black': Dict(
                     {
-                        'B boardmen': Box(low=low, high=high),
-                        'B barmen': Box(low=0,high=1),
-                        'B men off': Box(low=0,high=1),
+                        'B boardmen': Box(low=low, high=high,dtype=np.float32),
+                        'B barmen': Box(low=0,high=1,dtype=np.float32),
+                        'B men off': Box(low=0,high=1,dtype=np.float32),
                         'B turn': Discrete(2) # 1 if it is Black's turn, 0 if not
                     }
                 )
             }
         )
 
-        self.action_space =
-
-
-
-    # Constructing Observations From Environment States
-    def _get_obs(self):
-        pass
-
-    def _get_info(self):
-        pass
+        # self.observation_space and self.action_space merely specify the format of valid observations and actions for the benefit of humans. They are not necessary for the functioning of the environment(?)
+        # Since the action space is very complicated (it changes depending on current board configuration and the result of the random dice roll ), I make no attempt at specifying it and simply set it to 'None' instead
+        self.action_space = None
 
     # Reset
-    def reset(self):
-        pass 
+    def reset(self): 
+        # 'coin flip' to determine which player goes first
+        # if random() > 0.5:
+        #     w_turn = 1
+        #     b_turn = 0
+        # else:
+        #     w_turn = 0
+        #     b_turn = 1
+        # pass 
+
+        # observation = {
+        #     'White'
+        # }
 
     # Step
     def step(self, action):
