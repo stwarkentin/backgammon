39b056bcdd2c06df9c9acd348cb7f3bed560e061
stwarkentin <stwarkentin@uos.de>
2022-07-25 14:51:33 +0200

    Add class skeleton

---
+++ code/gym-backgammon/gym_backgammon/envs/backgammon.py
@@ -0,0 +1,30 @@
+class BackgammonEnv(gym.Env):
+    # Declaration and Initialization
+    def __init__(self):
+
+    # Constructing Observations From Environment States
+    def _get_obs(self):
+        pass
+
+    def _get_info(self):
+        pass
+
+    # Reset
+    def reset(self):
+        pass 
+
+    # Step
+    def step(self, action):
+        pass
+
+    # Rendering
+    def render(self):
+        pass
+
+    def _render_frame(self):
+        pass
+
+    # Close
+    def close(self):
+        pass
+ 
