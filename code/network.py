d25dc8988802ea86c8b4ae63b0aa682aa5b20f40
stwarkentin <stwarkentin@uos.de>
2022-07-26 12:16:27 +0200

    Improve __init__() method

--- code/network.py
+++ code/network.py
@@ -5,18 +5,14 @@ class Network(tf.keras.Model):
   def __init__(self, hidden_units=0):
     super().__init__()
 
-    self.output_layer = tf.keras.layers.Dense(4, activation=tf.nn.sigmoid)
+    self.layers = []
 
     if hidden_units != 0:
         self.hidden_layer = tf.keras.layers.Dense(hidden_units, activation=tf.nn.sigmoid)
-        self.layers = [
-            self.hidden_layer,
-            self.output_layer
-        ]
-    else:
-        self.layers = [
-            self.output_layer
-        ]
+        self.layers.append(self.hidden_layer)
+
+    self.output_layer = tf.keras.layers.Dense(4, activation=tf.nn.sigmoid)
+    self.layers.append(self.output_layer)
 
   def call(self, x):
     for layer in self.layers:
