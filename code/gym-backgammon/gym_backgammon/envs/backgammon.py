d9e95044f1741334db83e15b2929272a1ca5d192
stwarkentin <stwarkentin@uos.de>
2022-07-29 15:34:52 +0200

    Remove unused code, remove comments, combine 'pos' and 'barmen' into 'board' to facilitate actions

--- code/gym-backgammon/gym_backgammon/envs/backgammon.py
+++ code/gym-backgammon/gym_backgammon/envs/backgammon.py
@@ -71,7 +71,6 @@ class BackgammonEnv(gym.Env):
     # Reset
     def reset(self): 
 
-        # 'coin flip' to determine which side goes first
         coin = random() > 0.5
 
         self.state = {
@@ -82,7 +81,7 @@ class BackgammonEnv(gym.Env):
                 'turn': int(coin)
             },
             'B': {
-                'pos':starting_pos.reverse(), # Black's pieces mirror White's
+                'pos':starting_pos.reverse(),
                 'barmen': 0,
                 'menoff': 0,
                 'turn': 1-int(coin)
@@ -101,23 +100,13 @@ class BackgammonEnv(gym.Env):
         b_win = False
         w_gammon = False
         b_gammon = False
-        # what should actions look like?
-        # given a dice roll, we need to create a list of valid actions
-        # the action needs to contain: how many pieces will be moved, which pieces will be moved, how far the pieces will be moved
-        # from that, step() needs to calculate the results of those actions: moving pieces, blots, removing pieces from the board/bar,...
-        # ok so lets assume that an action is a list
-        # action = [[position_from_which_to_move_a_piece,how_many_spaces_to_move],[position_from_which_to_move_a_piece,how_many_spaces_to_move]]
-        # or
-        # action = [[position_from_which_to_move_a_piece,how_many_spaces_to_move],[position_from_which_to_move_a_piece,how_many_spaces_to_move],[position_from_which_to_move_a_piece,how_many_spaces_to_move],[position_from_which_to_move_a_piece,how_many_spaces_to_move]]
-        # problem: the bar is not a position per se? And I can't index it the way I'd index a board position because 'bar' is not part of the 'pos' array in the observation
+        
         for move in action:
             pass
-        
-        # switch turn order
+
         self.state['W']['turn'] = 1-self.state['W']['turn']
         self.state['B']['turn'] = 1-self.state['W']['turn']
 
-        # check for winning/gammon conditions
 
         if self.state['W']['menoff'] ==  1 and self.state['B']['pos']:
             w_gammon = True
@@ -126,11 +115,6 @@ class BackgammonEnv(gym.Env):
         elif 
         elif self.state['B']['menoff'] ==  1:
             b_win = True
-
-
-
-        # make observation
-
 
         #reward = 2 if w_gammon elif 1 if w_win elif -1 if b_win elif -2 if b_gammon else 0
         if w_gammon:
@@ -147,14 +131,3 @@ class BackgammonEnv(gym.Env):
         done = True if w_gammon or w_win or b_win or b_gammon else False
 
         return observation, reward, done,
-
-    # # Rendering
-    # def render(self):
-    #     pass
-
-    # def _render_frame(self):
-    #     pass
-
-    # # Close
-    # def close(self):
-    #     pass
