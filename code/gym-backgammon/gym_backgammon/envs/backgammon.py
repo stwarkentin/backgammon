6820b6488435c89897eb0123032dd6065838ca70
stwarkentin <stwarkentin@uos.de>
2022-08-01 20:47:28 +0200

    Add comments

--- code/gym-backgammon/gym_backgammon/envs/backgammon.py
+++ code/gym-backgammon/gym_backgammon/envs/backgammon.py
@@ -7,27 +7,37 @@ from copy import copy
 class BackgammonEnv(gym.Env):
     def __init__(self):
   
+        # Tesauro's backgammon algorithms encode the board using a specific 'truncated unary encoding' with some quirks. You can read more about it here:
         # https://en.wikipedia.org/wiki/Unary_coding 
         # http://www.scholarpedia.org/article/User:Gerald_Tesauro/Proposed/Td-gammon
         # https://www.bkgm.com/rgb/rgb.cgi?view+610
-   
-        low = np.zeros((97, 1))
-        high = np.ones((96, 1))
 
-        for i in range(3, 97, 4):
+        # although its purported benefits are not immediately apparent to us (perhaps we've been spoiled by the advances in computational power which have made such optimizations optional?),
+        # our encoding is largely based on the one used in Tesauro's 'pubeval.c', with the only changes made being the order of the values, and perhaps the use of two such figurative boards 
+        # (one for each player) rather than one 
+
+        # defining some ranges for our observation space
+        low = np.zeros((97, 1)) # 1 value for the bar + 4 per each of the 24 positions on the board
+        high = np.ones((96, 1)) # the first three of the four values encoding the number of checkers at a given position are either 0 or 1...
+
+        for i in range(3, 97, 4): # but every fourth value can go as high as (15-3)/2 = 6...
             high[i] = 6.0
 
-        high.insert(0,7.5)
+        high.insert(0,7.5) # and the very first value, which encodes the number of checkers on the bar, can go as high as 15/2 = 7.5
 
+        # to make them more readable to humans, our observations are dictionaries
+        # but we can't feed dictionaries to our ANN, so we are going to have to use gym's 'FlattenObservation' wrapper to flatten the dictionary into a single array later
         self.observation_space = Dict(
             {
+                # player 'White'
                 'W': Dict(
                     {
-                        'board': Box(low=low, high=high,dtype=np.float32),
-                        'menoff': Box(low=0,high=1,dtype=np.float32),
-                        'turn': Discrete(2)
+                        'board': Box(low=low, high=high,dtype=np.float32), # the board, consisting of the bar and 24 'points'
+                        'menoff': Box(low=0,high=1,dtype=np.float32), # number of checkers removed from the board as a fraction of the total number of checkers i.e. n/15
+                        'turn': Discrete(2) # '1' if it is this player's turn, '0' if not
                     }
                 ),
+                # player 'Black'
                 'B': Dict(
                     {
                         'board': Box(low=low, high=high,dtype=np.float32),
@@ -37,9 +47,13 @@ class BackgammonEnv(gym.Env):
                 )
             }
         )
-
+        
+        # which actions can be taken at any given time step depend on the state of the board and the roll of the dice. hence we choose to not attempt to define the action space
         self.action_space = None
-
+        
+        # create an empty board:
+        # to allow for indexing of board positions, the environment's state uses an array of arrays to store the values of the bar and each point
+        # later we simply flatten this array to create observations
         self.state = {
             'W': {
                 'board':np.zeros((24, 4)).insert(0,[0]), 
@@ -53,17 +67,22 @@ class BackgammonEnv(gym.Env):
             }
         }
 
+        # define the game's starting position:
+        # the previously mentioned truncated unary encoding is used to define the numbers of checkers found at certain points at the start of the game
         two = [1,1,0,0]
         three = [1,1,1,0]
         five = [1,1,1,1]
 
+        # create an empty board without a bar
         self.starting_pos = np.zeros((24, 4))
 
+        # place the correct number of checkers in the correct positions
         self.starting_pos[0] = five
         self.starting_pos[11] = two
         self.starting_pos[17] = five
         self.starting_pos[19] = three
 
+        # add the bar
         self.starting_pos.insert(0,[0])
 
     def _get_obs(self):
@@ -77,11 +96,10 @@ class BackgammonEnv(gym.Env):
 
     def reset(self): 
 
+        # a 'coin flip' to determine which side goes first
         coin = random() > 0.5
 
-        # W:    0 1 ... 24
-        # B:      24 ... 1 0
-
+        # reset the board to the game's starting position and assign a turn order based on the coin flip
         self.state = {
             'W': {
                 'board':starting_pos, 
@@ -97,22 +115,20 @@ class BackgammonEnv(gym.Env):
 
         return self._get_obs()
 
-    # translating from one board to the other: n+25-(2n)
-
     def step(self, action):
-        w_win = False
-        b_win = False
-        w_gammon = False
-        b_gammon = False
-        
+
         for move in action:
+            
             pass
 
+        # swap whose turn it is
         self.state['W']['turn'] = 1-self.state['W']['turn']
         self.state['B']['turn'] = 1-self.state['W']['turn']
 
+        # get observation
         observation = self._get_obs()
 
+        # reward is zero unless one of four conditions is met
         reward = 0
 
         # White wins, Black is gammoned
@@ -127,7 +143,17 @@ class BackgammonEnv(gym.Env):
         # Black wins
         elif self.state['B']['menoff'] ==  1:
             reward = -1
-        
+
+        # if one of the four conditions is met, the game is finished and the episode ends
         done = reward != 0
 
         return observation, reward, done
+
+        
+        
+        
+        
+ # W:    0 1 ... 24
+# B:      24 ... 1 0
+
+# translating from one board to the other: n+25-(2n)
