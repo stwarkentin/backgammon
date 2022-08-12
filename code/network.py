95ace107a048f0ed8fcbee7ab0bc7e2674241422
stwarkentin <stwarkentin@uos.de>
2022-08-12 16:05:39 +0200

    Initialize weights as uniform random values in the range [-0.5,+0.5]

--- code/network.py
+++ code/network.py
@@ -1,17 +1,20 @@
 import tensorflow as tf
+from tf.keras.initializers import RandomUniform
 
 class Network(tf.keras.Model):
 
   def __init__(self, hidden_units=0):
     super().__init__()
 
+    initializer = RandomUniform(minval=-0.05, maxval=0.05, seed=None)
+
     self.list_of_layers = []
 
     if hidden_units != 0:
-        self.hidden_layer = tf.keras.layers.Dense(hidden_units, activation=tf.nn.sigmoid)
+        self.hidden_layer = tf.keras.layers.Dense(hidden_units, activation=tf.nn.sigmoid, kernel_initializer = initializer)
         self.list_of_layers.append(self.hidden_layer)
 
-    self.output_layer = tf.keras.layers.Dense(4, activation=tf.nn.sigmoid)
+    self.output_layer = tf.keras.layers.Dense(4, activation=tf.nn.sigmoid, kernel_initializer = initializer)
     self.list_of_layers.append(self.output_layer)
 
   def call(self, x):
