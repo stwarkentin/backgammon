7efbe02382a60d2820dce83d375851852cd42d47
stwarkentin <stwarkentin@uos.de>
2022-08-25 20:30:07 +0200

    'Merge' network stuff

--- code/network.py
+++ code/network.py
@@ -1,8 +1,5 @@
 import tensorflow as tf
-<<<<<<< HEAD
-=======
 import numpy as np
->>>>>>> 450b74e327a896e01c3ca605cde72621f4626dac
 from tensorflow.keras.initializers import RandomUniform
 
 class Network(tf.keras.Model):
@@ -21,37 +18,27 @@ class Network(tf.keras.Model):
         self.output_layer = tf.keras.layers.Dense(4, activation=tf.nn.sigmoid, kernel_initializer = initializer)
         self.list_of_layers.append(self.output_layer)
 
-<<<<<<< HEAD
     def call(self, x):
         for layer in self.list_of_layers:
             x = layer(x)
         return x
 
-    def get_weights(self):
-        weights = []
-        for layer in self.list_of_layers:
-            weights.append(layer.get_weights())
-        
-        return weights
+    # def get_weights(self):
+    #     weights = []
+    #     for layer in self.list_of_layers:
+    #         weights.append(layer.get_weights())
+    #     return weights
         
 #     def set_weights(self):
-        
-=======
-    weights = np.empty(0)
-    for layer in self.list_of_layers:
-      np.append(weights, layer.get_weights())
-    self.weights_shape = len(weights.flatten())
+
+    # weights = np.empty(0)
+    # for layer in self.list_of_layers:
+    #   np.append(weights, layer.get_weights())
+    # self.weights_shape = len(weights.flatten())
 
   def call(self, x):
     for layer in self.list_of_layers:
         x = layer(x)
     return x
 
-  def test(self):
-    test = self.build([192,1])
-    for layer in self.list_of_layers:
-      print("weights:", len(layer.weights))
-      print("trainable_weights:", len(layer.trainable_weights))
-      print("non_trainable_weights:", len(layer.non_trainable_weights))
 
->>>>>>> 450b74e327a896e01c3ca605cde72621f4626dac
