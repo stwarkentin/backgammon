c1a57476abdcb31f0de1e9cdd047918dd25ca2bf
stwarkentin <stwarkentin@uos.de>
2022-08-29 17:30:11 +0200

    Add shebang, improved checkpoints

--- code/main.py
+++ code/main.py
@@ -1,24 +1,34 @@
+#!/usr/bin/env python 3
 from network import Network
 from agent import TDAgent
 from env.backgammon import BackgammonEnv
 import numpy as np
+import os
 from tqdm import tqdm
 
 
 env = BackgammonEnv()
-network = Network()
+
+hidden_units = 0
+network = Network(hidden_units)
 network.build([1,198])
+
 alpha = 0.1
 lmbd = 0.7
 gamma = 1
 agent = TDAgent(env, network, alpha, lmbd, gamma)
+
 episodes = 50000
 moves_per_game = np.asarray([])
+
+
 for i in tqdm (range(episodes), desc = "Learning..."):
     n_moves = agent.learn()
     np.append(moves_per_game,n_moves)
+    if (i+1) % 100 == 0:
+        np.save("n_moves.npy", moves_per_game)
+        filename = "n_units"+str(hidden_units)+"episode"+str(i+1)
+        network.save_weights('checkpoints/TDAgent/'+filename)
 
-np.save("n_moves.npy", moves_per_game)
-network.save_weights('0checkpoint50000')
 print("Done")
 
