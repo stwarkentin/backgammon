01a24f8cb0fce0753f5958a6aacb8b269012e543
marie <marie@debian-BULLSEYE-live-builder-AMD64>
2022-09-25 09:54:52 +0200

    final profiling

--- code/profiler.py
+++ code/profiler.py
@@ -1,5 +1,6 @@
-import numpy as np
-from tqdm import tqdm
+import cProfile
+import pstats
+from pstats import SortKey
 
 from network import Network
 from agent import DQNAgent
@@ -7,42 +8,37 @@ from env.backgammon import BackgammonEnv
 from memory import ReplayBuffer
 
 def main():
-    env = BackgammonEnv()
-
-    # build the model
-    network = Network()
-    network.build([1,198])
     
-    completed_episodes = 0
-    episodes = 60
-
-    # inititate the agentalpha = 0.1
-    memory = ReplayBuffer(100000)
+    # inititate either DQN-agent
+    env = BackgammonEnv()
+    network = Network()
     gamma = 0.99
     lr = 0.01
     epsilon = 0.9
     min_epsilon = 0.1
     epsilon_decay = 0.99999
-    batch_size = 60 
+    memory = ReplayBuffer(100000)
+    batch_size = 20 
+    episodes = 10
     agent = DQNAgent(env, network, gamma, lr, epsilon, min_epsilon, epsilon_decay, memory, batch_size)
 
-
-    for i in tqdm (range(episodes), desc = "Learning..."):
+    # training loop
+    for i in range(episodes):
         agent.learn()
-        completed_episodes += 1
-        print(completed_episodes)
+
+    agent.memory.save()
         
 if __name__ == "__main__":
-    import cProfile
+    
+    # run main() and colect profiling date
     cProfile.run('main()', "output.dat")
     
-    import pstats
-    from pstats import SortKey
-    
-    with open("output_time_dqn_fix.txt", "w") as f:
+    # sort data by amount of time
+    with open("DQN_10_output_time.txt", "w") as f:
         p = pstats.Stats("output.dat", stream=f)
         p.sort_stats("time").print_stats()
         
-    with open("output_calls_dqn_fix.txt", "w") as f:
+    # sort data by amount of calls
+    with open("DQN_10_output_calls.txt", "w") as f:
         p = pstats.Stats("output.dat", stream=f)
         p.sort_stats("calls").print_stats()
